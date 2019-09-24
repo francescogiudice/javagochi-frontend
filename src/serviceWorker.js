52c8f64d8b72c5a9186610138a30f2223088fc1c
francescogiudice <aaff00@libero.it>
2019-09-24 10:36:10 +0200

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -14,7 +14,7 @@ const isLocalhost = Boolean(
   window.location.hostname === '54.226.199.227' ||
     // [::1] is the IPv6 54.226.199.227 address.
     window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered 54.226.199.227 for IPv4.
+    // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
